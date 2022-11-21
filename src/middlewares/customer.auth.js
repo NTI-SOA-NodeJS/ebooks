fe6ab8c65199437cfda7868fa6e6520ff0a8bcb3
ahmadelzohairy <ahmadelzohairy93@gmail.com>
2022-11-21 13:42:58 +0200

    WIP

--- src/middlewares/customer.auth.js
+++ src/middlewares/customer.auth.js
@@ -4,7 +4,7 @@ const config = require("../config");
 exports.customer = (req, res, next) => {
   console.log("hello from super admin ");
   console.log(req.body.roleId);
-  if (req.body.roleId === 1) {
+  if (req.body.roleId === 3 && req.body.roleId === 2 && req.body.roleId === 1) {
     next();
   } else {
     res.json({
