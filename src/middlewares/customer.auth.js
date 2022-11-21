d36b35bea6d2b3211272bbde59dc23338c3a2fc6
ahmadelzohairy <ahmadelzohairy93@gmail.com>
2022-11-21 09:26:57 +0200

    feat: finish auth middleware

---
+++ src/middlewares/customer.auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config");
+
+exports.customer = (req, res, next) => {
+  console.log("hello from super admin ");
+  console.log(req.body.roleId);
+  if (req.body.roleId === 1) {
+    next();
+  } else {
+    res.json({
+      status: "failed",
+      message: "authorized only for customers and above",
+    });
+  }
+};
